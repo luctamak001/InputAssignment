eaa7d1afaa52f18c05028552015b52d318481541
Makenley Luctamar <luctamak001@warren.k12.in.us>
2024-09-10 08:29:43 -0400

    INIT

---
+++ main.py
@@ -0,0 +1,10 @@
+name = input("What is your name: ")
+droids = input("HOW MANY DROID DO YOU WANT TO SEE")
+wookiees = input("HOW MANY WOOKIES AND STORE AS WOOKIEES")
+#INSTRUCTION:  write an input
+# statement that asks for
+# number of droid and store as droids
+#INSTRUCTION:  write an input
+# statement that asks for
+# number of wookies and store as wookiees
+print(name, "wants to meet", droids, "droids and ", wookiees, "Wookiees!")
